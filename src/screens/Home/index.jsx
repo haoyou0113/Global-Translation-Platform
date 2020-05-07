2dc511cf60ddc7804b2362838858e8e7b72679dc
Ryota <34767371+ryotma14@users.noreply.github.com>
2020-05-07 13:56:13 +1000

    old one

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -7,14 +7,14 @@ import Gallery from './components/Gallery';
 import Menu from './components/Menu';
 import TopContent from './components/TopContent';
 import { get, post } from '../../utils/request';
-import Login from '../../pages/Login';
+import Login from '../../pages/login';
 import './index.css';
 const { Title } = Typography;
 
 const { Header, Footer, Sider, Content } = Layout;
 
 export const Home = () => {
-  const [authority, setAuthority] = useState(false);
+  const [authority, setAuthority] = useState(true);
 
   const login = (value) => {
     post('http://localhost:8080/api/user/login', value).then((res) => {
