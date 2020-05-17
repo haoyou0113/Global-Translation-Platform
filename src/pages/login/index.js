e24417f01d77b494e0b484b3cc336d2e30cc1ed6
YouHao <haoyou0807@163.com>
2020-05-18 01:20:21 +1000

    login debug

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,3 @@
-
 import RegistrationForm from './Rigester';
 import React, { Fragment, useState } from '../../../node_modules/react';
 import { Form, Input, Button, Checkbox } from '../../../node_modules/antd';
@@ -17,13 +16,6 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-
-import React, { Fragment, useState } from '../../../node_modules/react';
-import { Form, Input, Button } from '../../../node_modules/antd';
-import RegistrationForm from './Rigester';
-
-
-
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
@@ -34,7 +26,7 @@ const tailLayout = {
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant='body2' color='textSecondary' align='center'>
       {'CopyRight made by QUT Team 72 @2020 '}
     </Typography>
   );
@@ -48,7 +40,9 @@ const useStyles = makeStyles((theme) => ({
     backgroundImage: 'url(https://source.unsplash.com/random)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
-    theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   },
@@ -74,8 +68,6 @@ const useStyles = makeStyles((theme) => ({
 export default function Login(props) {
   const classes = useStyles();
 
-const Login = (props) => {
-
   const [reg, setReg] = useState(false);
   const onFinish = (values) => {
     console.log(values);
@@ -90,102 +82,77 @@ const Login = (props) => {
 
   return (
     <Fragment>
+      <Grid container component='main' className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <RegistrationForm reg={reg} regSwitch={regSwitch} />
+            <Avatar
+              className={classes.avatar}
+              style={{ display: reg ? 'none' : 'flex' }}
+            >
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography
+              component='h1'
+              variant='h5'
+              style={{ paddingBottom: 25, display: reg ? 'none' : 'block' }}
+            >
+              Sign in
+            </Typography>
+            <Form
+              style={{ display: reg ? 'none' : 'block' }}
+              {...layout}
+              name='basic'
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              initialValues={{ remember: true }}
+            >
+              <Form.Item
+                label='Username'
+                name='username'
+                rules={[
+                  { required: true, message: 'Please input your username!' },
+                ]}
+              >
+                <Input />
+              </Form.Item>
 
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-        <RegistrationForm reg={reg} regSwitch={regSwitch}/>
-          <Avatar className={classes.avatar}  style={{ display: reg ? 'none':'flex'}} >
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5" style={{ paddingBottom: 25, display: reg ? 'none' : 'block' }} >
-            Sign in
-          </Typography>
-          <Form
-            style={{ display: reg ? 'none' : 'block' }}    
-            {...layout}
-            name='basic'
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            initialValues={{ remember: true }}
-          >
-          <Form.Item
-            label='Username'
-            name='username'
-            rules={[{ required: true, message: 'Please input your username!' }]}
-          >
-            <Input />
-          </Form.Item>
+              <Form.Item
+                label='Password'
+                name='password'
+                rules={[
+                  { required: true, message: 'Please input your password!' },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item
+                {...tailLayout}
+                name='remember'
+                valuePropName='checked'
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
 
-          <Form.Item
-            label='Password'
-            name='password'
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-         </Form.Item>
+              <Form.Item {...tailLayout}>
+                <Button type='primary' htmlType='submit' size='medium' block>
+                  Sign In
+                </Button>
 
-          <Form.Item {...tailLayout}>
-            <Button type='primary' htmlType='submit' size = 'medium' block>
-              Sign In
-            </Button>
-
-            <Button onClick={regSwitch} size = 'medium' block >
-              {' '}
-              Sign Up?
-            </Button>
-
-        </Form.Item>
-      </Form>      
-        <Box mt={5}>
-          <Copyright />
-        </Box>
-        </div>
+                <Button onClick={regSwitch} size='medium' block>
+                  {' '}
+                  Sign Up?
+                </Button>
+              </Form.Item>
+            </Form>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
-
-      <RegistrationForm reg={reg} regSwitch={regSwitch} />
-      <Form
-        style={{ display: reg ? 'none' : 'block' }}
-        {...layout}
-        name='basic'
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label='Username'
-          name='username'
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label='Password'
-          name='password'
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-          <Button type='primary' htmlType='submit'>
-            Submit
-          </Button>
-          <Button onClick={regSwitch} style={{ marginLeft: 5 }}>
-            {' '}
-            Register
-          </Button>
-        </Form.Item>
-      </Form>
-
     </Fragment>
   );
 }
-
-
